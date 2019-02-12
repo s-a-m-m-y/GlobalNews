f2b7add4e03dcdb35111ab2c49083d712fd32d23
Sam <samuel.h.littlefield>
2019-02-11 22:44:58 -0500

    Create article model

--- project/globalnews/news/models.py
+++ project/globalnews/news/models.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+class Article(models.Model):
+    sumbitter = models.ForeignKey(User)
+    submitted_date = models.DateTimeField(default=now, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+            return self.title
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
