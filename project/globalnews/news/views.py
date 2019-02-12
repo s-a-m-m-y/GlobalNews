1cb39a605cc45e20bfceab820e47616e12b85318
Sam <samuel.h.littlefield>
2019-02-11 23:03:21 -0500

    Add index template w/ articles

--- project/globalnews/news/views.py
+++ project/globalnews/news/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from news.models import Article
+
 # Create your views here.
+def articles(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'news/articles.html', context)
